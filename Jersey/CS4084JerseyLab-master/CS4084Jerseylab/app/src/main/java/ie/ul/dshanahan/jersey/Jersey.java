de69e995f6e0ae5d8b5aea402cb924734e33b596
16230256 <39533070+16230256@users.noreply.github.com>
2018-10-22 22:00:54 +0100

    Jersey Lab at last

---
+++ Jersey/CS4084JerseyLab-master/CS4084Jerseylab/app/src/main/java/ie/ul/dshanahan/jersey/Jersey.java
@@ -0,0 +1,102 @@
+package ie.ul.dshanahan.jersey;
+
+import java.util.HashMap;
+import java.util.Random;
+
+import ie.ul.dshanahan.jersey.R;
+
+/**
+ * Created by Deirdre Shanahan 22/10/2018.
+ */
+public class Jersey {
+
+    private String mPlayerName = "ANDROID";
+    private int mPlayerId = 17;
+    private String mJerseyType;
+    private boolean mJerseyTypeGreen;
+    private boolean mJerseyTypePurple;
+
+    private int mImageResourceId;
+    private Random random = new Random();
+
+    public static final HashMap<String, Integer> sJerseyImageMap;
+
+    static {
+        sJerseyImageMap = new HashMap<>();
+
+        sJerseyImageMap.put("Green Hurling Jersey", R.drawable.green_jersey);
+        sJerseyImageMap.put("Purple Hurling Jersey", R.drawable.purple_jersey);
+
+
+    }
+
+    public Jersey() {
+
+
+        mJerseyType = getRandomJerseyName();
+        mImageResourceId = sJerseyImageMap.get(mJerseyType);
+
+        if (mJerseyType == "Green Hurling Jersey") {
+            mJerseyTypeGreen = true;
+            mJerseyTypePurple = false;
+        }
+
+        if (mJerseyType == "Purple Hurling Jersey") {
+            mJerseyTypeGreen = false;
+            mJerseyTypePurple = true;
+        }
+
+    }
+
+    public Jersey(String name, Integer number) {
+
+        setName(name);
+        setPlayerNumber(number);
+
+        mJerseyType = getRandomJerseyName();
+        mImageResourceId = sJerseyImageMap.get(mJerseyType);
+
+        if (mJerseyType == "Green Hurling Jersey") {
+            mJerseyTypeGreen = true;
+            mJerseyTypePurple = false;
+        }
+
+        if (mJerseyType == "Purple Hurling Jersey") {
+            mJerseyTypeGreen = false;
+            mJerseyTypePurple = true;
+        }
+
+    }
+
+    public static Jersey getDefaultJersey() {
+        return new Jersey();
+    }
+
+    ;
+
+
+    private String getRandomJerseyName() {
+        Object[] jerseys = Jersey.sJerseyImageMap.keySet().toArray();
+        return (String) jerseys[random.nextInt(jerseys.length)];
+    }
+
+    public String getName() {
+        return mPlayerName;
+    }
+
+    public int getPlayerNumber() {
+        return mPlayerId;
+    }
+
+    public void setName(String PlayerName) {
+        mPlayerName = PlayerName;
+    }
+
+    public void setPlayerNumber(int PlayerNumber) {
+        mPlayerId = PlayerNumber;
+    }
+
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+}
